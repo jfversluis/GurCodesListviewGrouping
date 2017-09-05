2c984da737200fb835835c52aa7e66cad831f962
Gerald Versluis <gerald@verslu.is>
2017-09-05 15:31:53 +0200

    Initial commit

---
+++ GurCodesListviewGrouping/GurCodesListviewGroupingPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using GurCodesListviewGrouping.Models;
+using GurCodesListviewGrouping.Services;
+using MvvmHelpers;
+using Xamarin.Forms;
+using System.Linq;
+
+namespace GurCodesListviewGrouping
+{
+    public partial class GurCodesListviewGroupingPage : ContentPage
+    {
+        public ObservableCollection<Grouping<string, Superhero>> Items { get; set; } = new ObservableCollection<Grouping<string, Superhero>>();
+
+        public GurCodesListviewGroupingPage()
+        {
+            InitializeComponent();
+
+            //foreach (var h in SuperheroService.GetSuperheroes())
+            //Items.Add(h);
+
+            var items = from hero in SuperheroService.GetSuperheroes()
+                        orderby hero.Name
+                        group hero by hero.Name[0].ToString().ToUpperInvariant() into heroGroup
+                        select new Grouping<string, Superhero>(heroGroup.Key, heroGroup);
+
+            foreach (var g in items)
+                Items.Add(g);
+
+            BindingContext = this;
+        }
+    }
+}
---
+++ GurCodesListviewGrouping/Models/Superhero.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace GurCodesListviewGrouping.Models
+{
+    public class Superhero 
+    {
+        [JsonProperty("superhero")]
+        public string Name { get; set; }
+
+		[JsonProperty("alter_ego")]
+        public string AlterEgo { get; set; }
+    }
+}
